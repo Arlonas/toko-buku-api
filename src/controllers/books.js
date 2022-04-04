ce4a8cbada7fde0f0b2df00e031b68f6747a65a0
Arlonas <hemapalaalfredo@gmail.com>
2022-04-04 15:58:44 +0700

    udh kelar bosssssss bisa bulkcreate akhirnyaaaaa ngerti

--- src/controllers/books.js
+++ src/controllers/books.js
@@ -1,33 +1,60 @@
-const { Book, Tag } = require("../lib/sequelize");
+const { Book, Tag, booksTag } = require("../lib/sequelize");
+const fileUploader = require("../lib/uploader");
 
 const bookControllers = {
   getAllBooks: async (req, res, next) => {
     try {
-      const { _limit = 3, _page = 1 } = req.query
+      const { _limit = 3, _page = 1 } = req.query;
 
-      delete req.query._limit
-      delete req.query._page
+      delete req.query._limit;
+      delete req.query._page;
 
       const findBooks = await Book.findAndCountAll({
         where: {
-          ...req.query
+          ...req.query,
         },
         limit: _limit ? parseInt(_limit) : undefined,
-        offset: (_page -1) * _limit,
+        offset: (_page - 1) * _limit,
         include: {
-           model: Tag
-        }
-      })
+          model: Tag,
+        },
+      });
 
       return res.status(200).json({
         message: "Find Book",
-        result: findBooks
-      })
+        result: findBooks,
+      });
     } catch (err) {
-      next()
+      next();
     }
   },
-  createBook: async (req, res, next) => {},
+  createBook: async (req, res, next) => {
+    const { book_name, stock } = req.body;
+    const uploadFileDomain = process.env.UPLOAD_FILE_DOMAIN;
+    const filePath = "book_images";
+    const { filename } = req.file;
+    const newBook = await Book.create({
+      book_name,
+      book_cover_img: `${uploadFileDomain}/${filePath}/${filename}`,
+      stock,
+    });
+
+    // {
+    // "tag": [
+    // 1,2,3,4
+    //    ]
+    //  }
+
+    const arrBookTags = req.body.tag.map((val) => {
+      return {
+        tag_id: val,
+        book_id: newBook.dataValues.id,
+      };
+    });
+    console.log(newBook);
+    // ([{newBook.Book.dataValues.id, tag.id}])
+    booksTag.bulkCreate(arrBookTags);
+  },
   updateBook: async (req, res, next) => {
     try {
       const { id } = req.params;
@@ -51,21 +78,21 @@ const bookControllers = {
     }
   },
   deleteBook: async (req, res, next) => {
-      try {
-          const { id } = req.params
+    try {
+      const { id } = req.params;
 
-          const deleteBook = await Book.destroy({
-              where: {
-                  id
-              }
-          })
+      const deleteBook = await Book.destroy({
+        where: {
+          id,
+        },
+      });
 
-          return res.status(200).json({
-              message: "Book deleted"
-          })
-      } catch (err) {
-          next()
-      }
+      return res.status(200).json({
+        message: "Book deleted",
+      });
+    } catch (err) {
+      next();
+    }
   },
 };
 
--- src/routes/books.js
+++ src/routes/books.js
@@ -1,10 +1,19 @@
-const { tagControllers, bookControllers } = require("../controllers")
+const { tagControllers, bookControllers } = require("../controllers");
+const fileUploader = require("../lib/uploader");
 
-const router = require("express").Router()
+const router = require("express").Router();
 
-router.get("/", bookControllers.getAllBooks)
-router.post("/", bookControllers.createBook)
-router.patch("/:id", bookControllers.updateBook)
-router.delete("/:id", bookControllers.deleteBook)
+router.get("/", bookControllers.getAllBooks);
+router.post(
+  "/",
+  fileUploader({
+    destinationFolder: "books",
+    fileType: "image",
+    prefix: "BOOK",
+  }).single("book_image_file"),
+  bookControllers.createBook
+);
+router.patch("/:id", bookControllers.updateBook);
+router.delete("/:id", bookControllers.deleteBook);
 
-module.exports = router
+module.exports = router;
